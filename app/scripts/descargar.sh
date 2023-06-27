ea0acebee4efc8b3a9203d2180fde74476a409b0
Ezequiel Mensur <eze-mensur@hotmail.es>
2023-06-27 15:56:35 -0300

    Fix(descarga): correccionpara no interrumpir el codigo cuando no encuentra la url

--- app/scripts/descargar.sh
+++ app/scripts/descargar.sh
@@ -9,13 +9,18 @@ function descargar_imagenes(){
     fi
 
     carpeta_destino="../download/imagenes"
-    url_imagenes=$1
-    url_suma_verificacion=$2
+    url_imagenes="$1"
+    url_suma_verificacion="$2"
     archivo_imagenes="imagenes.zip"
 
-    curl -o "$carpeta_destino/$archivo_imagenes" "$url"
+    wget --timeout=5 --tries=1 -O "$carpeta_destino/$archivo_imagenes" "$url_imagenes" || {
+        return 1
+    }
+    
+    suma_verificacion_descargada=$(curl --silent "$url_suma_verificacion") || {
+        return 1
+    }
 
-    suma_verificacion_descargada=$(curl --silent "$url_suma_verificacion")
     suma_verificacion_calculada=$(sha256sum "$carpeta_destino/$archivo_imagenes" | awk '{print $1}')
 
     if [ "$suma_verificacion_descargada" == "$suma_verificacion_calculada" ]; then
--- app/scripts/menu_seleccion.sh
+++ app/scripts/menu_seleccion.sh
@@ -15,18 +15,19 @@ mostrar_menu() {
 
     case $opcion in
         1)
-	    urlImagenes=$(whiptail --inputbox "Ingresar url de imagenes:" 10 60 3>&1 1>&2 2>&3)
-            urlHash=$(whiptail --inputbox "Ingresar url de hash:" 10 60 3>&1 1>&2 2>&3)
-	    eliminar_archivos_carpeta "./../download/archivos"
-            if descargar_imagenes $urlImagenes $urlHash; then
-                eliminar_archivos_carpeta "./../download/imagenes"
-                descomprimir_archivo
-                whiptail --title "Descarga y verificacion finalizada" --msgbox "Se han descargado correctamente las imagenes" 10 60
-            else
-                whiptail --title "Descarga y verificacion no finalizada" --msgbox "No se han descargado correctamente las imagenes" 10 60
-            fi
-            source menu_seleccion.sh
-	    ;;
+           urlImagenes=$(whiptail --inputbox "Ingresar URL de imágenes:" 10 60 3>&1 1>&2 2>&3)
+           urlHash=$(whiptail --inputbox "Ingresar URL de hash:" 10 60 3>&1 1>&2 2>&3)
+	   eliminar_archivos_carpeta "./../download/archivos"
+
+	   if descargar_imagenes "$urlImagenes" "$urlHash"; then
+    	      eliminar_archivos_carpeta "./../download/imagenes"
+    	      descomprimir_archivo
+    	      whiptail --title "Descarga y verificación finalizada" --msgbox "Se han descargado y verificado las imágenes." 10 60
+	   else
+    	      whiptail --title "Descarga y verificación no finalizada" --msgbox "No se pudo completar la descarga y verificación de las imágenes." 10 60
+	   fi
+	   source menu_seleccion.sh
+	   ;;
         2)
             valor=$(whiptail --inputbox "Cantidad de imágenes a generar:" 10 60 3>&1 1>&2 2>&3)
             if is_number $valor; then
